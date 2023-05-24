64bda571e94d447017b61aed774394ad13e95a5e
Rodrigo De la vega <delavegardr@gmail.com>
2023-05-24 09:59:00 -0300

    Primer commit. Se genera un mapa y se pueden buscar direcciones

---
+++ js/apiGeocoder.js
@@ -0,0 +1,42 @@
+const backend = 'http://localhost:8080/api';
+
+export const getDataGeocoder = ( dir ) => {
+    
+    const url  = backend + '/busquedaDireccion/' + dir;
+    const data = fetch( url )
+        .then( response => response.json() )
+        .then( response => {
+            return response;
+        } );
+    return data;  
+    // console.log( resp );
+
+    // return ({
+    //     "direccion": {
+    //         "departamento": {
+    //             "idDepartamento": 1,
+    //             "nombre_normalizado": "MONTEVIDEO"
+    //         },
+    //         "localidad": {
+    //             "idLocalidad": 3180,
+    //             "nombre_normalizado": "MONTEVIDEO"
+    //         },
+    //         "calle": {
+    //             "idCalle": 8419,
+    //             "nombre_normalizado": "JOSE B. FREIRE"
+    //         },
+    //         "numero": {
+    //             "nro_puerta": 129
+    //         }
+    //     },
+    //     "codigoPostal": 11900,
+    //     "codigoPostalAmpliado": 11915,
+    //     "puntoX": -56.21921410496764,
+    //     "puntoY": -34.854495387858634,
+    //     "idPunto": 380219,
+    //     "srid": 4326,
+    //     "idTipoClasificacion": 1,
+    //     "error": ""
+    // });
+
+}
