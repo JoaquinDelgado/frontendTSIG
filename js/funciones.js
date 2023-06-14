005d83774f89da8389eb2ed76955643c8acc229b
Rodrigo De la vega <delavegardr@gmail.com>
2023-06-14 01:43:21 -0300

    Dejo funcionando la busqueda parametrizada y el dibujo de los marcadores en el mapa

--- js/funciones.js
+++ js/funciones.js
@@ -1,45 +1,94 @@
-//Devuelve las relaciones 1 FC --> N Geocoders
-import {getGeoCoders} from "./apiGeocoder.js";
-import {getFCGeoCoders} from "./apiGeocoder.js";
+import { esDepartamento, esLocalidad } from "./departamentosLocalidades.js";
 
+export const getFCNormalizada = (fc) => {
+  return fc.replaceAll(",", ", ");
+};
 
-//export const getDir = (idGeoCoder, idFormaCanonica) => {
-const fcMap = new Map();
+export const getHttpQueryParams = (fc, input) => {
 
-const geocodersJson = getGeoCoders();
-console.log("tero")
-console.log(geocodersJson)
-const listIdGeocoder = Object.keys(geocodersJson)
+  const makeQueryParams = new Map([
+    ["1", makeQueryParamsFC1],
+    ["2", makeQueryParamsFC2],
+    ["3", makeQueryParamsFC3],
+    ["4", makeQueryParamsFC4],
+    ["5", makeQueryParamsFC5],
+    ["6", makeQueryParamsFC6],
+  ]);
 
-for (let i in listIdGeocoder ) {
-  const idGeocoder = listIdGeocoder[i];
-  const fcDelGeocoder = await getFCGeoCoders()
-  console.log(fcDelGeocoder) 
-  fcMap.set(idGeocoder, fcDelGeocoder);
+  if ( !makeQueryParams.has( fc ) ){
+    throw new Error('No se encontro la FC ' + fc);
+  }
+
+  //Obtengo la funcion a ejecutar segun la fc
+  const functionToRun = makeQueryParams.get(fc);
+
+  //TRABAJAR CON EL INPUT DEL USUARIO
+  let queryParams = functionToRun( input );
+
+  return queryParams;
+};
+
+//Los parametros del backend son 
+// calle
+// numero
+// localidad
+// departamento
+// calle2
+// manzana
+// solar
+// nombreInmueble
+// numeroRuta
+// kilometro
+const makeHttpQueryParams = ( direccion ) => {
+  let queryParams = '';
+  let separador = '';
+  for (const key in direccion) {
+    queryParams += separador + key + "=" + direccion[key];
+
+    separador = '&';
+  }
+
+  return queryParams;
 }
-   
-console.log(fcMap);
 
-//}
-        
-        
-                    /////////////////////////////
-/*
-// Agregar elementos a la lista en el Map
-fcMap.set("clave1", ["valor1", "valor2", "valor3"]);
-fcMap.set("clave2", ["valor4", "valor5"]);
+const makeQueryParamsFC1 = ( input ) => {
+  console.log('soy la FC 1');
+};
 
-// Acceder a la lista en el Map
-const lista1 = fcMap.get("clave1");
-console.log(lista1); // Imprime: ["valor1", "valor2", "valor3"]
+const makeQueryParamsFC2 = ( input ) => {
+  console.log('soy la FC 2');
+};
 
-// Agregar un elemento a la lista existente
-lista1.push("valor4");
-console.log(lista1); // Imprime: ["valor1", "valor2", "valor3", "valor4"]
+const makeQueryParamsFC3 = ( input ) => {
+  console.log('soy la FC 3');
+};
 
-// Iterar sobre la lista en el Map
-fcMap.forEach((lista, clave) => {
-  console.log(clave + ": " + lista);
+const makeQueryParamsFC4 = ( input ) => {
+  console.log('soy la FC 4');
+};
+
+const makeQueryParamsFC5 = ( input ) => {
+  console.log('soy la FC 5');
+};
+
+const makeQueryParamsFC6 = ( input ) => {
+  let splitInput = input.split(" ");
+
+  //Controlo errores
+  //Solo ingreso un nro o una calle o escribio todo pegado sin espacios
+  if ( splitInput.length <= 1 ){ 
+    throw new Error( 'Formato de dirección incorrecto' );
+  }
+
+  //Busco primero el nro
+  const indexNro = splitInput.length - 1;
+  const nro = splitInput[ indexNro ];
+  //El resto es la calle
+  splitInput.splice(indexNro, 1); //elimino el nro del array
+  const calle = splitInput.join(" "); //vuelvo a formar la calle
+
+  //Ahora que tengo los parametros construyo el httpQueryParams
+  const httpQueryParams = makeHttpQueryParams( {"calle": calle, "numero": nro} );
   
-});
-*/
+  return httpQueryParams;
+};
